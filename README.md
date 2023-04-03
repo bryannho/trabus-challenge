17eab87ec3690b589033fd108a0edaffdd459c7b
Bryan Ho <bryannho@usc.edu>
2023-04-02 18:08:38 -0700

    2.1 data ingestor

--- README.md
+++ README.md
@@ -4,3 +4,28 @@ Name: Bryan Ho \
 E-mail: bryan.ho16@gmail.com
 
 Coding challenge for Full Stack Engineer role at Trabus Technologies.
+
+## INSTRUCTIONS:
+
+### Data Ingestor
+
+0. Install dependencies \
+>>> pip3 install psychopg2
+
+1. Create an account with ElephantSQL, and create a new database instance. \
+
+2. Navigate to the "Details" tab within your new instance. \
+
+3. In your terminal, navigate to the ingestor/ directory. \
+>>> cd ingestor
+
+4. Open ingest.py in your code editor \
+
+5. Fill in "DB_NAME, DB_USER, ... DB_PORT" using the information from your "Details" tab on your ElephantSQL DB. \
+
+6. Run ingest.py \
+>>> python3 ingest.py
+
+7. In your ElephantSQL instance, navigate to the "Browser" tab and verify the data using:
+`SELECT * FROM climate`
+
---
+++ ingestor/ingest.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import psycopg2
+
+DB_NAME = "********"
+DB_USER = "********"
+DB_PASSWORD = "***********************"
+DB_HOST = "*******.db.elephantsql.com"
+DB_PORT = "5432"
+
+
+conn = psycopg2.connect(
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT
+)
+
+def createDB():
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS climate (station_id VARCHAR(4), date TIMESTAMP, temperature float, dewpoint float, wind_speed float, precipitation float)");
+    cur.execute("INSERT INTO climate VALUES('kmlb', '2019-01-01 01:00:00', 69.0, 67.0, 0.0, 0.0)")
+    conn.commit()
+    cur.close()
+
+# def insertDB(stationId, values):
+#     cur = conn.cursor()
+#     date, temp, dp, ws, precip = tuple(values)
+#     cur.execute("INSERT INTO climate VALUES(%s, %s, %s, %s, %s, %s)", (stationId, date, temp, dp, ws, precip))
+#     conn.commit()
+#     cur.close()
+
+# Connect to DB and insert batch of rows into DB
+def insertRowsToDB(values):
+    cur = conn.cursor()
+    cur.executemany("INSERT INTO climate VALUES(%s, %s, %s, %s, %s, %s)", values)
+    conn.commit()
+    cur.close()
+
+# Open CSV file, parse values, and insert to DB
+def ingestFile(fileName, stationId):
+    with open(fileName) as f:
+        allValues = []
+        for line in f.readlines()[1:]:
+            vals = line.strip().split(',')
+            for i in range(len(vals)):
+                if vals[i] == '':
+                    vals[i] = None
+            vals.insert(0, stationId)
+            allValues.append(vals)
+            print('Added: ', vals)
+        insertRowsToDB(allValues)
+
+def main():
+    # Create DB
+    createDB()
+    # Read Melbourne, FL climate data and insert into DB
+    ingestFile('data/kmlb_hrly_vals_2019.csv', 'kmlb')
+    # Read San Diego, CA climate data and insert into DB
+    ingestFile('data/ksan_hrly_vals_2019.csv', 'ksan')
+    # Close DB connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
